d44c0eb430a50d50226e61431a7cb3271a3fc73e
Danil Rodionov <Danil_Rodionov@epam.com>
2024-01-27 18:56:59 +0400

    feat: adjust project & add some utils

---
+++ packages/date-formatters/src/index.ts
@@ -0,0 +1,24 @@
+import dayjs from "dayjs";
+import localeRU from "dayjs/locale/ru.js";
+import { DateTime, DateTimeOptions } from "luxon";
+
+dayjs.locale(localeRU);
+
+const capitalize = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+};
+
+export const formatToDate = (timestamp: number | string) => {
+  return capitalize(dayjs(timestamp).format("dd DD.MM.YYYY HH:mm"));
+};
+
+export const formatToTimestamp = (date: string) => {
+  return +new Date(date);
+};
+
+export const formatStringDateToTimestamp = (
+  date: string,
+  options?: DateTimeOptions
+) => {
+  return DateTime.fromFormat(date, "yyyy-MM-dd HH:mm", options).toMillis();
+};
---
+++ packages/utils/src/index.ts
@@ -0,0 +1,5 @@
+export const getNonNullableValues = <T>(
+  array: (T | null | undefined)[]
+): T[] => {
+  return array.filter((v) => v != null) as T[];
+};
