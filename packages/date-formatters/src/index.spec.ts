e435bcc882612b0731fb8fa5b8841a8068f4c77d
Danil Rodionov <Danil_Rodionov@epam.com>
2024-01-28 15:35:28 +0400

    fix(tests): use TZ env to set the same timezone for local and ci tests

--- packages/date-formatters/src/index.spec.ts
+++ packages/date-formatters/src/index.spec.ts
@@ -1,7 +1,7 @@
 import assert from 'assert/strict';
 import { formatStringDateToTimestamp, formatToDate, formatToTimestamp } from './index.js';
 
-const mockDate = '2024-01-28 04:00';
+const mockDate = '2024-01-28 00:00';
 const mockTimestamp = 1706400000000;
 
 describe('formatToTimestamp', () => {
@@ -13,17 +13,17 @@ describe('formatToTimestamp', () => {
 
 describe('formatToDate', () => {
   it('return capitalized localized date in default format', () => {
-    const actualResult = formatToDate(1706400000000);
-    assert.deepStrictEqual(actualResult, 'Вс 28.01.2024 04:00');
+    const actualResult = formatToDate(mockTimestamp);
+    assert.deepStrictEqual(actualResult, 'Вс 28.01.2024 00:00');
   });
 
   it('return no capitalized localized date in default format', () => {
-    const actualResult = formatToDate(1706400000000, { capitalizeRes: false });
-    assert.deepStrictEqual(actualResult, 'вс 28.01.2024 04:00');
+    const actualResult = formatToDate(mockTimestamp, { capitalizeRes: false });
+    assert.deepStrictEqual(actualResult, 'вс 28.01.2024 00:00');
   });
 
   it('return localized date in custom format', () => {
-    const actualResult = formatToDate(1706400000000, { template: 'dd DD.MM' });
+    const actualResult = formatToDate(mockTimestamp, { template: 'dd DD.MM' });
     assert.deepStrictEqual(actualResult, 'Вс 28.01');
   });
 });
